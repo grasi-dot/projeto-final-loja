4386f3732a81407c652020ff1ae651863cc575f8
grasi-dot <grasieduarda775@gmail.com>
2024-12-15 20:39:19 -0300

    adjust

--- index.js
+++ index.js
@@ -1,42 +1,194 @@
+const track = document.querySelector('.carouselTrack');
+const items = document.querySelectorAll('.carouselItem');
+const prevButton = document.querySelector('.carouselButton.prev');
+const nextButton = document.querySelector('.carouselButton.next');
 const list = document.querySelector(".list");
+const tabs = document.querySelectorAll('.tabLink');
+const tabContents = document.querySelectorAll('.tabContent');
+const apotecaryList = document.querySelector(".apotecaryList");
+const jujutsuList = document.querySelector(".jujutsuList");
+const soloList = document.querySelector(".soloList");
+const searchBar = document.getElementById("searchBar");
+const searchButton = document.getElementById("searchButton");
 
 let best_sellers = [
   {
     id: 1,
-    name: "Solo Leveling Volume 3 Novel",
-    image: "solo-novel-3.webp",
+    name: "Solo Leveling Volume 1",
+    image: "solo-manhua-1.webp",
     category: "Solo Leveling",
     price: 6990
   },
   {
-    id: 18,
-    name: "Solo Leveling Volume 3 Manhua",
-    image: "solo-manhua-3.webp",
+    id: 2,
+    name: "Solo Leveling Volume 2",
+    image: "solo-manhua-2.webp",
     category: "Solo Leveling",
     price: 6990
   },
   {
     id: 3,
+    name: "Solo Leveling Volume 3",
+    image: "solo-manhua-3.webp",
+    category: "Solo Leveling",
+    price: 6990
+  },
+  {
+    id: 4,
     name: "Jujutsu Kaisen Volume 2",
     image: "jujutsu-2.jpeg",
     category: "Jujutsu Kaisen",
     price: 3690
   },
   {
-    id: 4,
+    id: 5,
     name: "Jujutsu Kaisen Volume 3",
     image: "jujutsu-3.jpeg",
     category: "Jujutsu Kaisen",
     price: 3690
   },
   {
-    id: 5,
+    id: 6,
     name: "Jujutsu Kaisen Volume 4",
     image: "jujutsu-4.jpeg",
     category: "Jujutsu Kaisen",
     price: 3690
   }
 ]
+
+let apotecarys = [
+  {
+    id: 1,
+    name: "Diarios de uma apotecária 1",
+    image: "apotecary-1.webp",
+    category: "Diarios de uma apotecária",
+    price: 4090
+  },
+  {
+    id: 18,
+    name: "Diarios de uma apotecária 2",
+    image: "apotecary-2.webp",
+    category: "Diarios de uma apotecária",
+    price: 4090
+  },
+  {
+    id: 3,
+    name: "Diarios de uma apotecária 3",
+    image: "apotecary-3.webp",
+    category: "Diarios de uma apotecária",
+    price: 4090
+  },
+  {
+    id: 4,
+    name: "Diarios de uma apotecária 4",
+    image: "apotecary-4.webp",
+    category: "Diarios de uma apotecária",
+    price: 4090
+  },
+]
+
+let jujutsus = [
+  {
+    id: 1,
+    name: "Jujutsu Kaisen 0",
+    image: "jujutsu-0.jpeg",
+    category: "Jujutsu Kaisen",
+    price: 4090
+  },
+  {
+    id: 18,
+    name: "Jujutsu Kaisen 1",
+    image: "jujutsu-1.jpeg",
+    category: "Jujutsu Kaisen",
+    price: 4090
+  },
+  {
+    id: 3,
+    name: "Jujutsu Kaisen 2",
+    image: "jujutsu-2.jpeg",
+    category: "Jujutsu Kaisen",
+    price: 4090
+  },
+  {
+    id: 4,
+    name: "Jujutsu Kaisen 3",
+    image: "jujutsu-3.jpeg",
+    category: "Jujutsu Kaisen",
+    price: 4090
+  },
+]
+
+let solos = [
+  {
+    id: 1,
+    name: "Solo Leveling Novel 1",
+    image: "solo-novel-1.webp",
+    category: "Solo Leveling",
+    price: 4090
+  },
+  {
+    id: 18,
+    name: "Solo Leveling Novel 2",
+    image: "solo-novel-2.webp",
+    category: "Solo Leveling",
+    price: 4090
+  },
+  {
+    id: 3,
+    name: "Solo Leveling Novel 3",
+    image: "solo-novel-3.webp",
+    category: "Solo Leveling",
+    price: 4090
+  }
+]
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const slideWidth = items[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % items.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  updateCarousel();
+}
+
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
+
+window.addEventListener('load', updateCarousel);
+
+setInterval(nextSlide, 10000);
+
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(t => t.classList.remove('active'));
+
+    tab.classList.add('active');
+    
+    tabContents.forEach(content => content.classList.remove('active'));
+
+    const target = document.getElementById(tab.dataset.tab);
+    target.classList.add('active');
+  });
+});
+
+searchButton.addEventListener("click", () => {
+  const query = searchBar.value.trim().toLowerCase();
+  if (query) {
+    localStorage.setItem("searchQuery", query);
+
+    window.location.href = "shopping.html";
+  }
+});
+
 
 const initApp = () => {
   best_sellers.forEach((best_seller, key) => {
@@ -62,4 +214,82 @@ const initApp = () => {
   });
 };
 
+const iniTabApotecary = () => {
+  apotecarys.forEach((apotecary, key) => {
+    const { image, category, name, price } = apotecary;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+      <img src="./img/store/${image}">
+      <div class="category">${category}</div>
+      <div class="title">${name}</div>
+      <div class="ratting">
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star-half'></i>
+      </div>
+      <button onclick="window.location.href='shopping.html'">
+        <i class='bx bx-cart iconCart'></i> Comprar agora
+      </button>
+    `;
+    apotecaryList.appendChild(newDiv);
+  });
+};
+
+const iniTabJujutsu = () => {
+  jujutsus.forEach((jujutsu, key) => {
+    const { image, category, name, price } = jujutsu;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+      <img src="./img/store/${image}">
+      <div class="category">${category}</div>
+      <div class="title">${name}</div>
+      <div class="ratting">
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star-half'></i>
+      </div>
+      <button onclick="window.location.href='shopping.html'">
+        <i class='bx bx-cart iconCart'></i> Comprar agora
+      </button>
+    `;
+    jujutsuList.appendChild(newDiv);
+  });
+};
+
+const initTabSolo = () => {
+  solos.forEach((solo, key) => {
+    const { image, category, name, price } = solo;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+      <img src="./img/store/${image}">
+      <div class="category">${category}</div>
+      <div class="title">${name}</div>
+      <div class="ratting">
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star-half'></i>
+      </div>
+      <button onclick="window.location.href='shopping.html'">
+        <i class='bx bx-cart iconCart'></i> Comprar agora
+      </button>
+    `;
+    soloList.appendChild(newDiv);
+  });
+};
+
 initApp();
+iniTabApotecary();
+iniTabJujutsu();
+initTabSolo();
+
+
+
---
+++ shopping.js
@@ -0,0 +1,144 @@
+const openShopping = document.querySelector("#bagIcon");
+const closeShopping = document.querySelector(".closeShopping");
+const list = document.querySelector(".list");
+const listCard = document.querySelector(".listCard");
+const total = document.querySelector(".total");
+const body = document.querySelector("body");
+const quantity = document.querySelector(".quantity");
+const searchBar = document.getElementById("searchBar");
+const searchButton = document.getElementById("searchButton");
+
+let listCards = [];
+let products = [];
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active")
+})
+
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active")
+})
+
+searchButton.addEventListener("click", () => {
+  const query = searchBar.value.trim().toLowerCase();
+  if (query) {
+    localStorage.setItem("searchQuery", query);
+
+    window.location.href = "shopping.html";
+  }
+});
+
+const handleAddToCart = (button, key) => {
+  addToCart(key);
+  button.innerHTML = "<i class='bx bx-loader-circle iconCart'></i> Adicionando...";
+
+  setTimeout(() => {
+    button.innerHTML = "<i class='bx bx-check-circle iconCart'></i> Adicionado!";
+
+    setTimeout(() => {
+      button.innerHTML = "<i class='bx bx-cart iconCart'></i> Adicionar";
+    }, 2000);
+  }, 2000);
+};
+
+const addToCart = (key) => {
+  if (!listCards[key]) {
+    const { name, image, price } = products[key];
+    listCards[key] = { name, image, price, quantity: 1 };
+  }
+  reloadCard();
+};
+
+const reloadCard = () => {
+  listCard.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+
+  listCards.forEach((item, key) => {
+    if (item) {
+      const { image, name, price, quantity } = item;
+      totalPrice += price * quantity;
+      count += quantity;
+
+      const newDiv = document.createElement("li");
+      newDiv.innerHTML = `
+        <div><img src="./img/store/${image}"></div>
+        <div class="cardTitle">${name}</div>
+        <div class="cardPrice">${(price / 100).toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</div>
+        <div>
+          <button style="background-color: #375a66" class="cardButton" onclick="changeQuantity(${key}, ${quantity - 1})">-</button>
+          <span>${quantity}</span>
+          <button style="background-color: #375a66" class="cardButton" onclick="changeQuantity(${key}, ++listCards[${key}].quantity)">+</button>
+        </div>
+      `;
+      listCard.appendChild(newDiv);
+    }
+  });
+
+  total.innerText = (totalPrice / 100).toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+  quantity.innerText = count;
+};
+
+const changeQuantity = (key, newQuantity) => {
+  newQuantity <= 0 ? delete listCards[key] : listCards[key].quantity = newQuantity;
+  reloadCard();
+};
+
+const renderProducts = (productList) => {
+  list.innerHTML = "";
+  productList.forEach((product, key) => {
+    const { image, category, name, price } = product;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+      <img src="./img/store/${image}">
+      <div class="category">${category}</div>
+      <div class="title">${name}</div>
+      <div class="ratting">
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star'></i>
+        <i class='bx bxs-star-half'></i>
+      </div>
+      <div class="price">R$ ${(price / 100).toLocaleString("pt-BR", { minimumFractionDigits: 2 })}</div>
+      <button onclick="handleAddToCart(this, ${key})">
+        <i class='bx bx-cart iconCart'></i> Adicionar
+      </button>
+    `;
+    list.appendChild(newDiv);
+  });
+};
+
+const initApp = async () => {
+  try {
+    const response = await fetch('./products.json');
+
+    if (!response.ok) {
+      throw new Error('Erro ao carregar os produtos.');
+    }
+
+    products = await response.json();
+
+    const searchQuery = localStorage.getItem("searchQuery");
+    if (searchQuery) {
+      const filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchQuery) || 
+        product.category.toLowerCase().includes(searchQuery)
+      );
+      renderProducts(filteredProducts);
+      localStorage.removeItem("searchQuery");
+    } else {
+      renderProducts(products);
+    }
+  } catch (error) {
+    console.error('Erro ao inicializar a aplicação:', error.message);
+  }
+};
+
+
+initApp();
+
+
+
+
